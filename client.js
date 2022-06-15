4ad20a929b69497a1b22a2249f2b90f9e728c8e5
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-15 09:03:53 -0500

    complete base questions

--- client.js
+++ client.js
@@ -1,36 +1,3 @@
-const employees = [
-	{
-		name: 'Atticus',
-		employeeNumber: '2405',
-		annualSalary: '47000',
-		reviewRating: 3
-	},
-	{
-		name: 'Jem',
-		employeeNumber: '62347',
-		annualSalary: '63500',
-		reviewRating: 4
-	},
-	{
-		name: 'Scout',
-		employeeNumber: '6243',
-		annualSalary: '74750',
-		reviewRating: 5
-	},
-	{
-		name: 'Robert',
-		employeeNumber: '26835',
-		annualSalary: '66000',
-		reviewRating: 1
-	},
-	{
-		name: 'Mayella',
-		employeeNumber: '89068',
-		annualSalary: '35000',
-		reviewRating: 1
-	}
-];
-
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // This problem is massive! Break the problem down, take small steps, and test as you go.
@@ -38,6 +5,38 @@ const employees = [
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
+const employees = [
+	{
+		name: 'Atticus',
+		employeeNumber: '2405',
+		annualSalary: '47000',
+		reviewRating: 3,
+	},
+	{
+		name: 'Jem',
+		employeeNumber: '62347',
+		annualSalary: '63500',
+		reviewRating: 4,
+	},
+	{
+		name: 'Scout',
+		employeeNumber: '6243',
+		annualSalary: '74750',
+		reviewRating: 5,
+	},
+	{
+		name: 'Robert',
+		employeeNumber: '26835',
+		annualSalary: '66000',
+		reviewRating: 1,
+	},
+	{
+		name: 'Mayella',
+		employeeNumber: '89068',
+		annualSalary: '35000',
+		reviewRating: 1,
+	},
+];
 
 console.log(employees);
 
@@ -67,7 +66,7 @@ function employeeBonus(employee) {
 							employees[i].employeeNumber,
 							employees[i].annualSalary
 						)
-				)
+				),
 			};
 		}
 	}
